f3f2fcb5ff907911d64f78226c5cf811abe42a99
selim8437 <91761278+selim8437@users.noreply.github.com>
2023-05-20 11:46:00 +0200

    Updating an Entity

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -35,7 +35,7 @@ class MixController extends AbstractController
         ]);
     }
     #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
-    public function vote(VinylMix $mix, Request $request): Response
+    public function vote(VinylMix $mix, Request $request, EntityManagerInterface $entityManager): Response
     {
         $direction = $request->request->get('direction', 'up');
         if ($direction === 'up') {
@@ -43,6 +43,9 @@ class MixController extends AbstractController
         } else {
             $mix->setVotes($mix->getVotes() - 1);
         }
-        dd($mix);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_mix_show', [
+            'id' => $mix->getId(),
+        ]);
     }
 }
