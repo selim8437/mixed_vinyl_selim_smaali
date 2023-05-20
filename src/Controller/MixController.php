f73384411edafba205521fb5c8e1481511e41dee
selim8437 <91761278+selim8437@users.noreply.github.com>
2023-05-20 11:38:52 +0200

    Querying for a Single Entity for a "Show" Page

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 use App\Entity\VinylMix;
+use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,5 +25,16 @@ class MixController extends AbstractController
             $mix->getId(),
             $mix->getTrackCount()
         ));
+    }
+    #[Route('/mix/{id}', name: 'app_mix_show')]
+    public function show($id, VinylMixRepository $mixRepository): Response
+    {
+        $mix = $mixRepository->find($id);
+        if (!$mix) {
+            throw $this->createNotFoundException('Mix not found');
+        }
+        return $this->render('mix/show.html.twig', [
+            'mix' => $mix,
+        ]);
     }
 }
