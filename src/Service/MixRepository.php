19cc99e01da16ed6b369e2d41a377f9dce902f8a
selim8437 <91761278+selim8437@users.noreply.github.com>
2023-05-20 10:13:02 +0200

    Installing Doctrine: symfony/orm-pack

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Service;
-
 use Psr\Cache\CacheItemInterface;
 use Symfony\Bridge\Twig\Command\DebugCommand;
 use Symfony\Component\Console\Input\ArrayInput;
@@ -10,7 +8,6 @@ use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Twig\Loader\FilesystemLoader;
-
 class MixRepository
 {
     public function __construct(
@@ -20,8 +17,7 @@ class MixRepository
         private bool $isDebug,
         #[Autowire(service: 'twig.command.debug')]
         private DebugCommand $twigDebugCommand,
-    ) {
-    }
+    ) {}
     public function findAll(): array
     {
         /*
@@ -29,7 +25,8 @@ class MixRepository
         $this->twigDebugCommand->run(new ArrayInput([]), $output);
         dd($output);
         */
-        return $this->cache->get('mixes_data', function (CacheItemInterface $cacheItem) {
+        dd($this->githubContentClient);
+        return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
             $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
             $response = $this->githubContentClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json');
             return $response->toArray();
