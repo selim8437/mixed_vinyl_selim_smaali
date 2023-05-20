2c7f5993fffed1c1686874a27d40d74a469d3045
selim8437 <91761278+selim8437@users.noreply.github.com>
2023-05-20 11:28:24 +0200

    Custom Entity Methods & Twig Magic

--- src/Command/TalkToMeCommand.php
+++ src/Command/TalkToMeCommand.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Command;
-use App\Service\MixRepository;
+use App\Repository\VinylMixRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -15,7 +15,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class TalkToMeCommand extends Command
 {
     public function __construct(
-        private MixRepository $mixRepository
+        private VinylMixRepository $mixRepository
     )
     {
         parent::__construct();
